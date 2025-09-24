9b50bdd1fd09ca0bf653620d44046d0644d90028
zs-andy <andy070119@outlook.com>
2025-09-25 00:24:18 +0800

    Initial Commit

---
+++ JumpScare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JumpScare
+//
+//  Created by Andy Lyu on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JumpScare/JumpScareApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpScareApp.swift
+//  JumpScare
+//
+//  Created by Andy Lyu on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JumpScareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
